896f968291f2e4a68818b13bdba86faf9d6669fe
Fehintolu Samuel <fehintoluwasamuel@gmail.com>
2026-08-28 17:13:12 +0000

    Updated program.cs

--- src/NorthboundSessions.Web/Program.cs
+++ src/NorthboundSessions.Web/Program.cs
@@ -23,6 +23,9 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure()));
+
 builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure()));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
