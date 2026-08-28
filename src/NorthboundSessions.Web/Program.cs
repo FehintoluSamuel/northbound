20409871de9f1e345198dc75f2f2d0c0372255d1
Fehintolu Samuel <fehintoluwasamuel@gmail.com>
2026-08-28 16:44:55 +0000

    Updated the backend wiring

--- src/NorthboundSessions.Web/Program.cs
+++ src/NorthboundSessions.Web/Program.cs
@@ -40,6 +40,12 @@ builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSe
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
